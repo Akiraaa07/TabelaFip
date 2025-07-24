d165fa9cb656479a21b16120c88f758bbc8683c2
Akiraaa07 <igor.tateishi@gmail.com>
2025-07-24 11:19:32 -0300

    Projeto final – Spring Boot - API OMDb + Jackson + Streams

--- src/main/java/com/example/TabelaFipe/principal/Principal.java
+++ src/main/java/com/example/TabelaFipe/principal/Principal.java
@@ -1,5 +1,94 @@
 package com.example.TabelaFipe.principal;
 
+import com.example.TabelaFipe.model.Dados;
+import com.example.TabelaFipe.model.Modelos;
+import com.example.TabelaFipe.model.Veiculo;
+import com.example.TabelaFipe.service.ConsumoApi;
+import com.example.TabelaFipe.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Principal {
+    private Scanner leitura =  new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+
+    public void exibeMenu(){
+        String menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite umas das opções para consultar: 
+                """;
+
+        System.out.println(menu);
+        String opcao = leitura.nextLine();
+
+        String endereco;
+
+        if (opcao.toLowerCase().contains("carr")) {
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo.obterDados(endereco);
+        System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para a consulta: ");
+        String codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca.toLowerCase() + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("\nDigite uma trecho do nome do carro que deseja consultar: ");
+        String nomeCarro = leitura.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeCarro.toLowerCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("Modelos dessa marca: ");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("\nDigite o código do modelo prar consultar os valores de avaliação: ");
+        String codigoModelo = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumo.obterDados(endereco);
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for(int i = 0; i < anos.size(); i++) {
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = consumo.obterDados(enderecoAnos);
+            Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("\nTodos os modelos dessa marca: ");
+        veiculos.forEach(System.out::println);
+    }
 
 }
--- src/main/java/com/example/TabelaFipe/service/ConverteDados.java
+++ src/main/java/com/example/TabelaFipe/service/ConverteDados.java
@@ -2,6 +2,9 @@ package com.example.TabelaFipe.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
 
 public class ConverteDados implements IConverteDados {
 
@@ -15,6 +18,17 @@ public class ConverteDados implements IConverteDados {
             return mapper.readValue(json, classe);
         } catch (JsonProcessingException e) {
             // Se der erro ao tentar ler o JSON (ex: JSON malformado), lança uma exceção genérica
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> List<T> obterLista(String json, Class<T> classe) {
+        CollectionType lista = mapper.getTypeFactory().constructCollectionType(List.class, classe);
+
+        try {
+            return mapper.readValue(json, lista);
+        } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
